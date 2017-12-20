5e8ef52dc8edb479fcd025ed1053895f8617b3f8
wanchun <1596900283@qq.com>
2017-12-20 16:21:41 +0800

    新增加了启动动画

--- app/src/main/java/com/wan/grace/graceplayer/MainActivity.java
+++ app/src/main/java/com/wan/grace/graceplayer/MainActivity.java
@@ -3,11 +3,22 @@ package com.wan.grace.graceplayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import site.gemus.openingstartanimation.NormalDrawStrategy;
+import site.gemus.openingstartanimation.OpeningStartAnimation;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        OpeningStartAnimation openingStartAnimation = new OpeningStartAnimation.Builder(this)
+                .setDrawStategy(new NormalDrawStrategy())
+                .setAppName("GracePlayer")
+                .setAppIcon(getResources().getDrawable(R.mipmap.ic_launcher))
+                .setAnimationFinishTime(100)
+                .setAnimationInterval(5000)
+                .create();
+        openingStartAnimation.show(this);
     }
 }
