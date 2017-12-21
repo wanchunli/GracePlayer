8063ddf28191d1aff7476e8660ceac99e3ca06b2
wanchun <1596900283@qq.com>
2017-12-21 11:16:37 +0800

    优化了启动动画

--- app/src/main/java/com/wan/grace/graceplayer/MainActivity.java
+++ app/src/main/java/com/wan/grace/graceplayer/MainActivity.java
@@ -3,22 +3,35 @@ package com.wan.grace.graceplayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.wan.grace.graceplayer.handler.HandlerUtil;
+import com.wan.grace.graceplayer.widget.SplashScreen;
+
 import site.gemus.openingstartanimation.NormalDrawStrategy;
 import site.gemus.openingstartanimation.OpeningStartAnimation;
 
 public class MainActivity extends AppCompatActivity {
 
+    private SplashScreen splashScreen;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        splashScreen = new SplashScreen(this);
+        splashScreen.show(R.mipmap.art_login_bg,
+                SplashScreen.CENTER_OUT);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        OpeningStartAnimation openingStartAnimation = new OpeningStartAnimation.Builder(this)
-                .setDrawStategy(new NormalDrawStrategy())
-                .setAppName("GracePlayer")
-                .setAppIcon(getResources().getDrawable(R.mipmap.ic_launcher))
-                .setAnimationFinishTime(100)
-                .setAnimationInterval(5000)
-                .create();
-        openingStartAnimation.show(this);
+//        OpeningStartAnimation openingStartAnimation = new OpeningStartAnimation.Builder(this)
+//                .setDrawStategy(new NormalDrawStrategy())
+//                .setAppName("GracePlayer")
+//                .setAppIcon(getResources().getDrawable(R.mipmap.ic_launcher))
+//                .setAnimationFinishTime(100)
+//                .setAnimationInterval(5000)
+//                .create();
+//        openingStartAnimation.show(this);
+        HandlerUtil.getInstance(this).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                splashScreen.removeSplashScreen();
+            }
+        }, 3000);
     }
 }
